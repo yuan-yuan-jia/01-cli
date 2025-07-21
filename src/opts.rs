012e3baca4530594c50a4eb33c221a25a45c5360
jyy <1305081045@qq.com>
2025-07-21 17:06:45 +0800

    feature: support generic csv to json and yaml output

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt, path::Path, str::FromStr};
 
 use clap::Parser;
 
@@ -15,12 +15,20 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
     #[arg(short, long, default_value_t = ',')] // default_value_t 后跟字面量值
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -32,5 +40,34 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format")),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
